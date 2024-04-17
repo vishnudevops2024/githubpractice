4bddf8bba046cf90d399b88cbcc02d1ed37854dc
vishnudevops2024 <vishnudevops2024>
2024-04-18 00:20:45 +0530

    varibles

--- 02-variable.sh
+++ 02-variable.sh
@@ -5,5 +5,5 @@ person2=Mayukha
 
 echo "$person1 : Hi , How Are You"
 echo "$person2 : Hi , I am Good"
-echo "$person1 : "Okay"
-echo "$person2 : "BYE"
+echo "$person1 : Okay"
+echo "$person2 : BYE"
