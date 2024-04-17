3ab09f852a0e63417c511089c5822d69f634f68b
vishnudevops2024 <vishnudevops2024>
2024-04-18 00:51:48 +0530

    varibles

--- 09-condition.sh
+++ 09-condition.sh
@@ -1,13 +1,11 @@
+
 #!/bin/bash
 
 NUMBER=$1
 
-if [$NUMBER -gt 10 ]
-    then
-
-    echo "given number $NUMBER is grater than 10"
-
+if [ $NUMBER -gt 10 ]
+then
+    echo "Given number $NUMBER is greater than 10"
 else
-    echo "given number $NUMBER is less than 10"
-
+    echo "Given number $NUMBER is less than 10" 
 fi
