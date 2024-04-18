f16e127c46ce5cbea4fddd851ed9ba52d6ec737c
vishnudevops2024 <vishnudevops2024>
2024-04-19 00:06:44 +0530

    varibles

--- 10-installation.sh
+++ 10-installation.sh
@@ -7,7 +7,30 @@ echo "USERID IS $USERID "
 if [ $USERID -ne 0 ]  
 then 
     echo "please run this script with root access"
+    exit 1
 else 
     echo "you are the Super User"
 
 fi
+
+dnf install mysql -y
+
+if [$? -ne 0]
+then
+    echo "installation of mysql ...FAILURE"
+    exit 1
+
+else 
+    echo "installation of mysql.. SUCCESS"
+fi
+
+dnf install git -y 
+
+if [$? -ne 0]
+then 
+    echo "installation of Git .. FAILURE"
+    exit 1
+else
+    echo "installation of Git.. SUCCESS"
+fi
+
