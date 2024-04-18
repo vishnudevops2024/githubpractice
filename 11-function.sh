25297f4ab587892b8e31769671f80dbe3a517500
vishnudevops2024 <vishnudevops2024>
2024-04-19 01:06:16 +0530

    varibles

---
+++ 11-function.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE()
+{
+    if [ $? -ne 0 ]
+    then 
+        echo "$2 .. FAILURE"
+        exit 1
+    else    
+        echo "$2.. SUCCESS"
+    fi
+
+}
+
+    if [ $USERID -ne 0 ]
+        then
+            echo "please run with root user"
+            exit 1
+        else
+            echo "you are super user"
+        fi
+
+dnf install mqsql -y
+VALIDATE $? "Installing MQSQL"
+
+dnf install git -y
+VALIDATE $? "installing Git"
