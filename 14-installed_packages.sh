db3a9da1adc36c10fc47e5d81d14ef0b043383cb
vishnudevops2024 <vishnudevops2024>
2024-04-23 22:48:07 +0530

    packages

--- 14-installed_packages.sh
+++ 14-installed_packages.sh
@@ -15,7 +15,7 @@ for i in $@
 do 
     echo "package to instal : $i"
     dnf list installed $i
-        if [ $? -eq 0]
+        if [ $? -eq 0 ]
         then
             echo "package installed..SKIPPING"
         else
