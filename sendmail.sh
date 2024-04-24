7cccf8b4dbee909e1aaf02cb37317141c7ab8f96
vishnudevops2024 <vishnudevops2024>
2024-04-24 23:39:02 +0530

    packages

--- sendmail.sh
+++ sendmail.sh
@@ -9,5 +9,33 @@ N=$"\e[0m"
 
 
 SUBJECT=HIGH_CPU_USAGE
-
 echo -e " $R $SUBJECT $N"
+
+EMAIL="vishnudevops2024@gmail.com"
+
+sendmailalert() {
+
+(
+    echo "TO : $EMAIL"
+    echo "FROM:vishnuvemula564@gmail.com"
+    echo -e "SUBJECT : $R $SUBJECT $N "
+    echo "Please check below word count"
+
+
+) | mail -s 
+
+}
+
+wordcount() {
+
+cat wordcount1.txt | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr | head -n 5 |
+  while read -r count word;  
+  do
+        echo -e  " '$word'  appears $count  times in file " 
+    done 
+    
+
+}
+
+wordcount
+
