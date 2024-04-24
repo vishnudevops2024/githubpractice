f0affd1c809a7a016e7270df6595377f18ad0d36
vishnudevops2024 <vishnudevops2024>
2024-04-24 23:59:21 +0530

    packages

---
+++ wordcount_html_color.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+wordcount() {
+    echo "<html>"
+    echo "<head>"
+    echo "<style>"
+    echo "  .red { color: red; }"
+    echo "  .green { color: green; }"
+    echo "</style>"
+    echo "</head>"
+    echo "<body>"
+
+    cat wordcount1.txt | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr | head -n 5 |
+    while read -r count word;  
+    do
+        echo "<p>'<span class=\"red\">$word</span>' appears <span class=\"green\">$count</span> times in file</p>"
+    done 
+
+    echo "</body>"
+    echo "</html>"
+}
+
+wordcount | mail -s "wordcount alert" -a "Content-Type: text/html" vishnudevops2024@gmail.com
