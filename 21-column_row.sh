ed7048426fe45d5582740e5b781e9b256341dc88
vishnudevops2024 <vishnudevops2024>
2024-04-24 19:06:50 +0530

    packages

--- 21-column_row.sh
+++ 21-column_row.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-cat column_row.txt | tr '\n'
+#cat column_row.txt | tr '\n'
+
+
+while IFS= read -r line; do
+    # Skip the header line
+    if [[ ! $line =~ ^Name ]]; then
+        # Convert each line into rows using awk
+        echo "$line" | awk '{print $1 "\t" $2}'
+    fi
+done < "column_row.txt"
