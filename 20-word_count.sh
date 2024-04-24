19a7502a0d764a3839307bafdfb87ca71354f93e
vishnudevops2024 <vishnudevops2024>
2024-04-24 23:28:49 +0530

    packages

--- 20-word_count.sh
+++ 20-word_count.sh
@@ -8,7 +8,7 @@ N=$"\e[0m"
 cat wordcount1.txt | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr | head -n 5 |
   while read -r count word;  
   do
-        echo -e  " $R'$word' $N appears $G $count $N times in file " | mail -s "wordcount" vishnudevops2024@gmail.com
+        echo -e  " $R'$word' $N appears $G $count $N times in file " 
     done 
     
     
---
+++ sendmail.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+R=$"\e[31m"
+N=$"\e[0m"
+SUBJECT=$($R HIGH_CPU_USAGE $N)
