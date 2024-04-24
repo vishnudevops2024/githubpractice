768713793f2704b4277ced34e122a215341b04e2
vishnudevops2024 <vishnudevops2024>
2024-04-24 18:42:43 +0530

    packages

--- 20-word_count.sh
+++ 20-word_count.sh
@@ -4,7 +4,8 @@
 
 ##tr '[:space:]' '[\n*]' < your_text_file.txt | grep -v "^\s*$" | sort | uniq -c | sort -bnr
 
-cat wordcount1.txt | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | echo "$1 is"
+cat wordcount1.txt | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr | head -n 5
+ 
 
 
 
