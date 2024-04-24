c5b5a36896f66c4b94b6b9cad69964263b16a53d
vishnudevops2024 <vishnudevops2024>
2024-04-24 23:54:59 +0530

    packages

--- sendmail.sh
+++ sendmail.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-
-
-R=$"\e[31m"
-G=$"\e[32m"
-N=$"\e[0m"
-
-
 wordcount() {
 
-echo "Content-Type : text\html"
-echo 
-echo "<html>"
-echo "<body>"
-echo "<h3 style='color:red'> wordcount checking </h3>"
-echo "sample test mail"
-echo "</body>"
-echo "</html>"
 cat wordcount1.txt | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr | head -n 5 |
   while read -r count word;  
   do
