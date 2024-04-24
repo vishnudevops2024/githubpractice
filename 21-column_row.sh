2bd8a538896c868ab745c641b145ae83326c7096
vishnudevops2024 <vishnudevops2024>
2024-04-24 22:05:59 +0530

    packages

--- 21-column_row.sh
+++ 21-column_row.sh
@@ -3,19 +3,27 @@
 #cat column_row.txt | tr '\n'
 
 # Transpose the input table using awk
-awk '{
-    for (i = 1; i <= NF; i++) {
-        if (NR == 1) {
+awk 
+'{
+    for (i = 1; i <= NF; i++)
+     {
+        if (NR == 1) 
+        {
             header[i] = $i;
-        } else {
+        } 
+        else 
+        {
             data[i][NR - 1] = $i;
         }
     }
 }
-END {
-    for (i = 1; i <= NF; i++) {
+END 
+{
+    for (i = 1; i <= NF; i++) 
+    {
         printf("%s\t", header[i]);
-        for (j = 1; j <= NR - 1; j++) {
+        for (j = 1; j <= NR - 1; j++) 
+        {
             printf("%s\t", data[i][j]);
         }
         printf("\n");
