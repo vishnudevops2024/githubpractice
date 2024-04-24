5442e71a3b02cd5f83728b4a6fc6355996b9f379
vishnudevops2024 <vishnudevops2024>
2024-04-24 18:56:21 +0530

    packages

--- 20-word_count.sh
+++ 20-word_count.sh
@@ -3,12 +3,12 @@
 R=$"\e[31m"
 G=$"\e[32m"
 N=$"\e[0m"
-B=$"\e[1m"
+
 
 cat wordcount1.txt | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr | head -n 5 |
-  while read -r count word;  
+  while IFS=read -r count word;  
   do
-        echo -e  " $R$B '$word' $N appears $G$B $count $N times in file "
+        echo -e  " $R'$word' $N appears $G $count $N times in file "
     done
 
 
