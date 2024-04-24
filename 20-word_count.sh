c0f0b5a9fb3af0d3ca4e2809a6693f8cc2a6674c
vishnudevops2024 <vishnudevops2024>
2024-04-24 18:54:39 +0530

    packages

--- 20-word_count.sh
+++ 20-word_count.sh
@@ -1,35 +1,18 @@
 #!/bin/bash
 
-##echo "Lorem ipsum dolor sit sit amet et cetera." | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr
-
-##tr '[:space:]' '[\n*]' < your_text_file.txt | grep -v "^\s*$" | sort | uniq -c | sort -bnr
-
 R=$"\e[31m"
 G=$"\e[32m"
 N=$"\e[0m"
+B=$"\e[1m"
 
 cat wordcount1.txt | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr | head -n 5 |
   while read -r count word;  
   do
-        echo -e  " $R '$word' $N appears $G $count $N times in file "
+        echo -e  " $B '$word' $N appears $B $count $N times in file "
     done
 
 
 
-
-
-#| sort | uniq -c | sort -bnr | head -n 5
-
-##while IFS= read -r line; 
-##do
-    # Convert each line into words, filter out empty lines, and count occurrences
-    ##echo "$line" | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr
-    # Iterate over each word/count pair and print in sentence format
-    #while read -r count word; 
-    #do
-        #echo "The word '$word' appears $count time(s) in the text."
-    #done
-##done < "wordcount1.txt" 
 
  
 
