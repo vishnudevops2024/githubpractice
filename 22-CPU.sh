3665400af54acf22de082e59ea3c494049193ef0
vishnudevops2024 <vishnudevops2024>
2024-04-25 00:25:28 +0530

    packages

--- 22-CPU.sh
+++ 22-CPU.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+HOSTNAME=$hostname
+SUBJECT="HIGH_CPU_USAGE"
 CPUUSAGE=$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6)
+sendmailalert(){
+    (
+        echo -e "SUBJECT : $SUBJECT"
+        echo "please check high CPU Usage"
+    ) | mail -s "Top 5 CPU Usage" vishnudevops2024@gmail.com
+}
 
-echo "$CPUUSAGE" | mail -s "Top 5 CPU Usage" vishnudevops2024@gmail.com
+THRESHOULD=0
+
+checkcpuusage(){
+
+        CPUUSAGE=$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6)
+        if [ $CPUUSAGE -gt $THRESHOULD]
+        then
+            echo "High cpu detected on $HOSTNAME"
+            sendmailalert
+
+        else
+            echo "CPU usage normal range"
+
+        fi
+
+}
+checkcpuusage
+
+
 
 
