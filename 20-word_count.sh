9be449e152c42eaea1005ea3138380999ec45086
vishnudevops2024 <vishnudevops2024>
2024-04-24 18:05:37 +0530

    packages

--- 20-word_count.sh
+++ 20-word_count.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "Lorem ipsum dolor sit sit amet et cetera." | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr
+##echo "Lorem ipsum dolor sit sit amet et cetera." | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr
+
+##tr '[:space:]' '[\n*]' < your_text_file.txt | grep -v "^\s*$" | sort | uniq -c | sort -bnr
+
+cat wordcount1.txt | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr
