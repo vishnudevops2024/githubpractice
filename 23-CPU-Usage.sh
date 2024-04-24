58990cd8b2839a1bb319e4aade151e91b8533b72
vishnudevops2024 <vishnudevops2024>
2024-04-25 01:14:50 +0530

    packages

--- 23-CPU-Usage.sh
+++ 23-CPU-Usage.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 SUBJECT="HIGH_CPU_USAGE"
 CPUUSAGE=$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6)
-HIGHUSAGE=$CPUUSAGE | grep "%cpu" | awk '{print $NF}' | cut -d' ' -f1 
 
-echo "$CPUUSAGE" 
-echo "$HIGHUSAGE" | mail -s "CPU USAGE " vishnudevops2024@gmail.com
+echo "$CPUUSAGE" | mail -s "CPU USAGE " vishnudevops2024@gmail.com
