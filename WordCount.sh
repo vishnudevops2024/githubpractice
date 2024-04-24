a777f05f68c475348b7dfaa79acc1efe687047ae
vishnudevops2024 <vishnudevops2024>
2024-04-25 01:07:12 +0530

    packages

--- WordCount.sh
+++ WordCount.sh
@@ -5,10 +5,10 @@ wordcount() {
 cat wordcount1.txt | tr '[:space:]' '[\n*]' | grep -v "^\s*$" | sort | uniq -c | sort -bnr | head -n 5 |
   while read -r count word;  
   do
-        echo -e  " '$word' appears $count  times in file " 
+        echo -e  " '$word' appears $count  times in text file " 
     done 
    
 
 }
-wordcount  | mail -s "WORDCOUNT-EXAMPLE1" vishnudevops2024@gmail.com
+wordcount  | mail -s "WORDCOUNT-EXAMPLE" vishnudevops2024@gmail.com info@devops.com
 
